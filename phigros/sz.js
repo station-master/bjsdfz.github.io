068890291788c06eb559e99907a08920da98cdd7
station-master <82510975+station-master@users.noreply.github.com>
2021-10-21 20:22:35 +0800

    Add files via upload

---
+++ phigros/sz.js
@@ -0,0 +1,23 @@
+
+var zsl=12;
+setTimeout(() => add("tap",60,10), 200);
+setTimeout(() => add("tap",20,10), 50);
+setTimeout(() => add("tap",50,10), 700);
+setTimeout(() => add("tap",70,10), 200);
+setTimeout(() => add("tap",30,10), 1550);
+setTimeout(() => add("tap",66,10), 1200);
+setTimeout(() => add("tap",49,10), 2300);
+setTimeout(() => add("tap",27,10), 590);
+setTimeout(() => add("tap",12,10), 5008);
+setTimeout(() => add("tap",33,10), 6900);
+setTimeout(() => add("tap",78,10), 3090);
+setTimeout(() => add("tap",82,10), 5500);
+
+
+
+
+
+
+
+
+
