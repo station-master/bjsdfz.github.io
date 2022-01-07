4a7f0e608a5fc970efd35a2ae8a0494c5b65dca4
station-master <82510975+station-master@users.noreply.github.com>
2022-01-07 20:28:27 +0800

    Update gn.js

--- qsy/gn.js
+++ qsy/gn.js
@@ -111,8 +111,11 @@ function ckxsfz(idm,w,h){                           //调整窗口大小
                         };
         
         }}
+
         function ckydcp(idm2){                                                  //窗口移动（触屏）
-            //alert("cp")
+          
+            
+            //alert("cp1")
             if(isInArray(ygd,idm2)==true){console.log("az")
             return}else{
         
@@ -122,22 +125,28 @@ function ckxsfz(idm,w,h){                           //调整窗口大小
             
             var oBox2 = document.getElementById(idd);
             var oBox1919810 = document.getElementById(idm2);
-                oBox2.touchStart = function(ert) {
+                oBox2.ontouchstart = function(ert) {
+                    if  (ert.targetTouches.length == 1) {
+                         ert.preventDefault(); 
+                                 }
                     var touch = ert.touches[0];
                     ckgd(idm2)
-                var disX=touch.pageX-oBox1919810.offsetLeft;
-                var disY=touch.pagey-oBox1919810.offsetTop;   
-                document.touchMove = function(ev114514) {
-                var l = ev114514.clientX-disX;
-                var t = ev114514.clientY-disY;
-                //console.log(l);
+                var disX=touch.clientX-oBox1919810.offsetLeft;
+                var disY=touch.clientY-oBox1919810.offsetTop; 
+                console.log("az")
+                //alert(disX)  
+                document.ontouchmove = function(ev114514) {
+                    var touch = ev114514.touches[0];
+                    var l = touch.clientX-disX;
+                    var t = touch.clientY-disY;
+                console.log(l);
                 if(isInArray(ygd,idm2)==true){//console.log("az")
                 return}else{
                 oBox1919810.style.left = l + "px";
                 oBox1919810.style.top = t + "px";}
                 };
-                                document.touchEnd = function() {
-                                document.touchMove = null;
+                                document.ontouchend = function() {
+                                document.ontouchmove = null;
                             };
                             
                                 return false;  
@@ -182,6 +191,7 @@ function ckxsfz(idm,w,h){                           //调整窗口大小
                         //ckgd("ck" + xrcks)
                     }
         }
+        
         function ckdxcp(idm){                                             //窗口大小（触屏）
             if(isInArray(ygd,idm)==true){
 
@@ -193,26 +203,30 @@ function ckxsfz(idm,w,h){                           //调整窗口大小
             //console.log(dxidm)
             var oBox = document.getElementById(dxidm);
             var oBox114514 = document.getElementById(idm);
-        oBox.onmousedown = function() {
+        oBox.ontouchstart = function(erty) {
+            if  (erty.targetTouches.length == 1) {
+                erty.preventDefault(); 
+                        }
             ckgd(idm)
 
           disX2=oBox114514.style.left.split("px")[0];
           disY2=oBox114514.style.top.split("px")[0];   
-        
-            document.onmousemove = function(ev) {
-            var l2 = ev.clientX-disX2;
-            var t2 = ev.clientY-disY2;
+          console.log("az233")
+            document.ontouchmove = function(ev) {
+                var touch = ev.touches[0];
+            var l2 = touch.clientX-disX2;
+            var t2 = touch.clientY-disY2;
             //console.log(l);
         //alert(l+"az"+t)
 
 
-        if(isInArray(ygd,idm)==true){console.log("az")
+        if(isInArray(ygd,idm)==true){
         return}else{
         ckxsfz(idm,l2+"px",t2+"px")}
         
             };
-                            document.onmouseup = function() {
-                            document.onmousemove = null;
+                            document.ontouchend = function() {
+                            document.ontouchmove = null;
                         };
                         
                             return false;  
@@ -237,13 +251,13 @@ function ckxsfz(idm,w,h){                           //调整窗口大小
             sourceNode.parentNode.appendChild(clonedNode);
 
             var ynr=""
-            document.getElementById("ck"+xrcks).innerHTML=ynr+"    <div class='tzk'   onmousemove=ckyd('ck"+xrcks+"') ontouchstart=ckydcp('ck"+xrcks+"') onmousedown=ckgd('ck"+xrcks+"') id='tzk-ck"+xrcks+"'></div><div class='ckbt'>"+ckbt+"</div>"
+            document.getElementById("ck"+xrcks).innerHTML=ynr+"    <div class='tzk'   onmousemove=ckyd('ck"+xrcks+"') ontouchmove=ckydcp('ck"+xrcks+"') onmousedown=ckgd('ck"+xrcks+"') id='tzk-ck"+xrcks+"'></div><div class='ckbt'>"+ckbt+"</div>"
             var ynr=document.getElementById("ck"+xrcks).innerHTML
             document.getElementById("ck"+xrcks).innerHTML=ynr+"    <div class='ckclose' onclick=azclose('ck"+xrcks+"')></div><div class='ckgd' onclick=azgd('ck"+xrcks+"') style='background-image: url(img/bqck/gd.png);' id='gd-ck"+xrcks+"'></div>"
             var ynr=document.getElementById("ck"+xrcks).innerHTML
             document.getElementById("ck"+xrcks).innerHTML=ynr+"<div class='beijin' style='background: "+bjys+";opacity: "+bjbtmd+";filter: blur("+ckbmh+");'></div>"
             var ynr=document.getElementById("ck"+xrcks).innerHTML
-            document.getElementById("ck"+xrcks).innerHTML=ynr+"<div id='nr-ck"+xrcks+"' style='width: 100%;height:0%;' class='xsbq'></div><div class='dxtjd' onmousemove=ckdx('ck"+xrcks+"')  id='dxk-ck"+xrcks+"'></div>"
+            document.getElementById("ck"+xrcks).innerHTML=ynr+"<div id='nr-ck"+xrcks+"' style='width: 100%;height:0%;' class='xsbq'></div><div class='dxtjd' ontouchmove=ckdxcp('ck"+xrcks+"') onmousemove=ckdx('ck"+xrcks+"')  id='dxk-ck"+xrcks+"'></div>"
             document.getElementById("nr-ck"+xrcks).innerHTML="<iframe src='"+src+"' border='0' frameborder='no' framespacing='0' allowfullscreen='true' width='100%' height='100%' class='yjiao'></iframe>"
 
             ckxs("ck"+xrcks,kd,gd)
@@ -303,21 +317,6 @@ touchStart 触屏按下
 
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
